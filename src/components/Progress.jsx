e53bae846534bdae41fc66700e764befddb09a92
Aishat Abidemi Waheed <waheedaishat@gmail.com>
2023-12-05 13:37:36 +0100

    updates

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -16,7 +16,7 @@ const Progress = ({ raised, total }) => {
   const progressValue = (raised / total) * 100;
 
   return (
-    <div className="flex flex-col space-y-4">
+    <div className="flex flex-col space-y-4 bg-[#EAECF0] rounded-[4px]">
       {/* <div className="flex items-center">
         <span className="mr-2 font-semibold text-gray-600">Amount Raised:</span>
         <span className="text-lg font-bold text-blue-500">₦{raised.toLocaleString()}</span>
